551b0a7ecbc3af895e39fb2273a06385348a0c69
Bahaa Assad <bahaa.asad@quizplus.com>
2023-08-22 13:48:04 +0300

    added jwt files

---
+++ demo/src/main/java/com/example/demo/config/ConfigConstants.java
@@ -0,0 +1,8 @@
+package com.example.demo.config;
+
+public class ConfigConstants {
+
+    private static final long serialVersionUID = -2550185165626007488L;
+
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+}
